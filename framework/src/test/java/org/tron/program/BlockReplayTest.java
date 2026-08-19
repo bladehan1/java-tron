c12bb71479f6a72414083005c02e41282ad19c40
bladehan1 <bladehan@163.com>
2026-08-19 10:59:07 +0800

    feat(block): replay consecutive block chunks

--- framework/src/test/java/org/tron/program/BlockReplayTest.java
+++ framework/src/test/java/org/tron/program/BlockReplayTest.java
@@ -63,6 +63,35 @@ public class BlockReplayTest {
   }
 
   @Test
+  public void shouldVerifyConsecutiveDirectoryWithGlobalLimitAndWarmup() throws Exception {
+    BlockId parent = new BlockId(Sha256Hash.ZERO_HASH, 9);
+    BlockCapsule[] blocks = blocks(parent, 10, 4);
+    Path directory = temporaryFolder.newFolder("chunks").toPath();
+    write(directory.resolve("second.dat"), new BlockCapsule[] {blocks[2], blocks[3]});
+    write(directory.resolve("first.dat"), new BlockCapsule[] {blocks[0], blocks[1]});
+
+    BlockReplay.ReplayResult result = BlockReplay.replayInput(directory, null, null, 1, 3, false);
+
+    assertTrue(result.format().contains("range=[10,12]"));
+    assertTrue(result.format().contains("processed=3"));
+    assertTrue(result.format().contains("warmup=1"));
+    assertTrue(result.format().contains("measured=2"));
+  }
+
+  @Test
+  public void shouldRejectParentMismatchBetweenChunks() throws Exception {
+    BlockId parent = new BlockId(Sha256Hash.ZERO_HASH, 9);
+    BlockCapsule first = blocks(parent, 10, 1)[0];
+    BlockCapsule second = blocks(new BlockId(Sha256Hash.ZERO_HASH, 10), 11, 1)[0];
+    Path directory = temporaryFolder.newFolder("parent-mismatch").toPath();
+    write(directory.resolve("10.dat"), new BlockCapsule[] {first});
+    write(directory.resolve("11.dat"), new BlockCapsule[] {second});
+
+    assertThrows(IllegalArgumentException.class,
+        () -> BlockReplay.replayInput(directory, null, null, 0, Long.MAX_VALUE, false));
+  }
+
+  @Test
   public void shouldApplyBlocksThroughSyncPath() throws Exception {
     BlockId parent = new BlockId(Sha256Hash.ZERO_HASH, 9);
     BlockCapsule[] blocks = blocks(parent, 10, 2);
@@ -125,6 +154,10 @@ public class BlockReplayTest {
 
   private Path write(BlockCapsule[] blocks) throws Exception {
     Path input = temporaryFolder.getRoot().toPath().resolve("blocks.dat");
+    return write(input, blocks);
+  }
+
+  private Path write(Path input, BlockCapsule[] blocks) throws Exception {
     BlockFile.write(input, blocks[0].getNum(), blocks[blocks.length - 1].getNum(), false,
         height -> {
           BlockCapsule block = blocks[(int) (height - blocks[0].getNum())];
